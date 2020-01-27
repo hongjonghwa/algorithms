39961019b546eef75b9e41c328708583e35b6c18
Hong Jong Hwa <jonghwa.hong@gmail.com>
2020-01-27 22:48:17 +0900

    이런저런

---
+++ fast-rand.c
@@ -0,0 +1,16 @@
+/*
+  https://software.intel.com/en-us/articles/fast-random-number-generator-on-the-intel-pentiumr-4-processor/
+*/
+
+static unsigned int g_seed;
+
+//Used to seed the generator.
+inline void fast_srand( int seed ){
+    g_seed = seed;
+}
+
+//fastrand routine returns one integer, similar output value range as C lib.
+inline int fastrand(){
+    g_seed = (214013*g_seed+2531011);
+    return (g_seed>>16)&0x7FFF;
+}
