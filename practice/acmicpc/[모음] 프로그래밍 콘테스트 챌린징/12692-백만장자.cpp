b815a030a9c347b5cecd40f7162d71f6fe7d69e1
Hong Jong Hwa <jonghwa.hong@gmail.com>
2021-05-07 04:31:18 +0000

    12692 ver1 -> timeout

--- practice/acmicpc/[모음] 프로그래밍 콘테스트 챌린징/12692-백만장자.cpp
+++ practice/acmicpc/[모음] 프로그래밍 콘테스트 챌린징/12692-백만장자.cpp
@@ -1,19 +1,48 @@
 #include<cstdio>
+#include<cstring>
+#include<cassert>
 
-int N, M, X;
-double P;
+template<class T>
+inline T min(const T a, const T b){
+    return a<b ? a : b;
+}
 
-double solv(){
-    printf("%d %lf %d\n", M,  P, X);
-    return 0;
+int T;
+
+double D[16][(1<<15) + 1];
+double solv(int round, double prob, int starting){
+    // printf("%d %lf %d\n", round,  prob, starting);
+    int n = (1<<round);
+    memset(D[0], 0, sizeof(D[0]));
+    D[0][n] = 1.0;
+    for (int r = 1 ; r <= round ; ++r){
+        assert(n < (1<<15) + 1);
+        D[r][n] = 1.0;
+        for (int i = 0 ; i < n ; ++i){
+            D[r][i] = 0;
+            int j_max = min(i, n-i);
+            for (int j = 0; j <= j_max ; j++){
+                double t = prob*D[r-1][i+j] + (1-prob)*D[r-1][i-j];
+                if (t>D[r][i]) D[r][i] = t;
+            }
+        }
+    }
+
+    // for (int i =0 ; i <= n ; ++i) printf(" - %d %lf\n", i, D[round][i]);
+
+    if (starting >= 1000000) return 1;
+
+    return D[round][starting * n / 1000000];
 }
 
 int main(){
-    if (scanf("%d", &N) != 1) return 1;
+    if (scanf("%d", &T) != 1) return 1;
 
-    for (int t = 0 ; t < N ; ++t){
-        if (scanf("%d%lf%d", &M, &P, &X) != 3) return 1;
-        double ans = solv();
-        printf("%lf\n", ans);
+    for (int t = 0 ; t < T ; ++t){
+        int n, x;
+        double p;
+        if (scanf("%d%lf%d", &n, &p, &x) != 3) return 1;
+        double ans = solv(n, p, x);
+        printf("Case #%d: %lf\n", t+1, ans);
     }
 }
