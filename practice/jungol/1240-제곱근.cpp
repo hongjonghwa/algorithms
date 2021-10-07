67a1d6b0bfc8bbeef694df065c24be2b17b7a886
Hong Jong Hwa <jonghwa.hong@gmail.com>
2021-10-07 16:15:00 +0900

    1007

--- practice/jungol/1240-제곱근.cpp
+++ practice/jungol/1240-제곱근.cpp
@@ -3,5 +3,13 @@
 using namespace std;
 
 int main() {
-	cout << "hello";
+	unsigned long long n;
+	cin >> n;
+	long long hi = 1ll<<32, lo = 0;
+	while (hi>lo+1){
+		long long mid = (hi+lo)/2;
+		if (mid * mid > n) hi = mid;
+		else lo = mid;
+	}
+	cout << lo << "\n";
 }
